e08c1e963339f28ab434e94933bbe6ef0555edab
IamJenver <jenver@yandex.ru>
2021-01-04 19:14:41 +0300

    add three_sity.py

--- three_sity.py
+++ three_sity.py
@@ -1,3 +1,21 @@
 first, second, therd = input(), input(), input()
-print(min(len(first), len(second), len(therd)))
-print(max(len(first), len(second), len(therd)))
+m1 = min(len(first), len(second), len(therd))
+m2 = max(len(first), len(second), len(therd))
+if m1 == len(first) and m2 == len(second):
+    print(first)
+    print(second)
+elif m1 == len(first) and m2 == len(therd):
+    print(first)
+    print(therd)
+elif m1 == len(second) and m2 == len(therd):
+    print(second)
+    print(therd)
+elif m1 == len(second) and m2 == len(first):
+    print(second)
+    print(first)
+elif m1 == len(therd) and m2 == len(first):
+    print(therd)
+    print(first)
+elif m1 == len(therd) and m2 == len(second):
+    print(therd)
+    print(second)
