06064eff5c8e82d16d146618c339d6322ccfe37e
IamJenver <jenver@yandex.ru>
2021-01-15 14:10:16 +0300

    factorial.py

---
+++ factorial.py
@@ -0,0 +1,7 @@
+# На вход программе подается натуральное число n.
+# Напишите программу, которая вычисляет n!
+n = int(input())
+counter = 1
+for i in range(1, n + 1):
+    counter *= i
+print(counter)
