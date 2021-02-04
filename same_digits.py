8b58ee45b97e6d5e8e85f5622548589a03e947dd
IamJenver <jenver@yandex.ru>
2021-02-04 19:05:21 +0300

    fix same_digits.py

--- same_digits.py
+++ same_digits.py
@@ -1,13 +1,13 @@
 # Дано натуральное число. Напишите программу, которая определяет, состоит ли указанное число из одинаковых цифр.
 num = int(input())
-averedge = 0
-counter = 0
+flag = True
+sample = num % 10
 while num != 0:
     last_digit = num % 10
-    averedge += last_digit
-    counter += 1
+    if sample != last_digit:
+        flag = False
     num = num // 10
-if averedge / counter == last_digit:
+if flag == True:
     print('YES')
 else:
     print('NO')
