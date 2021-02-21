e21d1db20a080960ca547d8120209869fd25168f
IamJenver <jenver@yandex.ru>
2021-02-21 19:38:42 +0300

    add str_palindron.py

--- palindrom_1.py
+++ palindrom_1.py
@@ -1,7 +1,8 @@
-# Дан список words. Составьте из него список слов палиндромов. 
-words = ['мадам', 'топот', 'test', 'madam', 'word']
+# Дан список my_str. Составьте из него список палиндромов. 
+my_str = ['Око за око', 'А роза упала на лапу Азора', 'Около Миши молоко']
 palindrome = []
-for el in words:
-    if el[::-1] == el:
+for el in my_str:
+    el_r = el.replace(' ', '').lower()
+    if el_r[::-1] == el_r:
         palindrome.append(el)
 print(palindrome)
